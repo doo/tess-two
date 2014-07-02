9c6ce093cd0aeb2d871741c4b05403ffcaf95f33
Alex Koch <alex@e42.com>
2014-07-02 10:16:40 +0200

    Deactivated mips and x86. Not yet supported with libjpeg-turbo

--- tess-two/jni/Application.mk
+++ tess-two/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_STL := gnustl_static
-APP_ABI := x86 armeabi armeabi-v7a #mips
+APP_ABI := armeabi armeabi-v7a #x86 mips  TODO support all plattforms http://stackoverflow.com/questions/12260149/libjpeg-turbo-for-android
 APP_OPTIM := release
 APP_PLATFORM := android-10
 APP_CPPFLAGS += -fexceptions -frtti
