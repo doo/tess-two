ed4fc93dcd5315c7e97f7f7481404e0ad589ca99
Stefan Dragnev <stefan@scanbot.io>
2019-01-09 13:09:28 +0100

    move tess-two project into the root

--- tess-two/jni/Application.mk
+++
@@ -1,6 +0,0 @@
-APP_STL := c++_shared
-APP_ABI := armeabi-v7a x86 arm64-v8a x86_64
-APP_OPTIM := release
-APP_PLATFORM := android-9
-APP_CPPFLAGS += -fexceptions -frtti
-NDK_TOOLCHAIN_VERSION := clang
